a6616baa21fa2110b452286050119a6b57e14ad4
c4605 <bolasblack@gmail.com>
2016-08-11 16:26:42 +0800

    support carthage

---
+++ i18nextiOS/i18nextiOS.h
@@ -0,0 +1,20 @@
+//
+//  i18nextiOS.h
+//  i18nextiOS
+//
+//  Created by c4605 on 16/8/9.
+//  Copyright © 2016年 i18next. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "I18Next.h"
+
+//! Project version number for i18nextiOS.
+FOUNDATION_EXPORT double i18nextiOSVersionNumber;
+
+//! Project version string for i18nextiOS.
+FOUNDATION_EXPORT const unsigned char i18nextiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <i18nextiOS/PublicHeader.h>
+
+
